b87e8fd31ebf88b157595bce5d41a7a3c957e2c5
Dcode22 <dovidylevine@gmail.com>
2023-11-21 21:05:42 +0200

    added apollo-angular and first graphql request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Apollo, gql } from 'apollo-angular';
 
+const GET_DATA_QUERY = gql`
+  query GetArticles {
+    getArticles {
+      headline
+      text
+      authorId
+    }
+  }
+`;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'aws-test';
+  constructor(private apollo: Apollo) {}
+
+  ngOnInit() {
+    this.apollo.watchQuery<any>({
+      query: GET_DATA_QUERY
+    })
+    .valueChanges
+    .subscribe(({ data, loading }) => {
+      console.log('data: ', data)
+    });
+  }
 }
